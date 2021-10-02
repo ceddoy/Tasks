3c42dde046efec069e4bb4d02610f13de287235e
ceddoy <66414293+ceddoy@users.noreply.github.com>
2021-10-02 17:57:33 +0700

    add PyLounge_task_1

---
+++ PyLounge_task_1.py
@@ -0,0 +1,34 @@
+import json
+
+
+def load_file(fuction):
+    """ Выгрузка из json в py."""
+    def wrapper(file_name):
+        with open(f"{file_name}.json", encoding="utf-8") as f:
+            result = json.load(f)
+            return fuction(result) # Результат возвращает в функцию averageSalaryByAge
+
+    return wrapper
+
+
+def average(dict_salaries):
+    """ Расчет средней заработной платы. """
+    for key, values in dict_salaries.items():
+        dict_salaries[key] = sum(values) / len(values)
+    return dict_salaries
+
+
+@load_file
+def averageSalaryByAge(data): # Передался список со вложенными словарями.
+    resultAvrSalary = {} # Здесь будем хранить отфильтрованный результат данных.
+    for data_dict in data:
+        new_key = str(data_dict.get('age')) # При каждой итерации, будем присваивать переменной, ключ по возрастам.
+        salary = data_dict.get('rate') # Аналогично, только тут будет значение (зарплата).
+        """ Тут работаем уже с добавлением зарплат по ключу. """
+        if new_key not in resultAvrSalary: # Здесь проверка на наличие возраста в результирующем словаре.
+            resultAvrSalary[new_key] = [salary] # Если True, то создаем новый ключ со значением зарплаты
+        else:                                   # списком, для хранения результата всех зарплат по возрасту.
+            resultAvrSalary[new_key].append(salary) # Есть уже такой ключ есть, то просто добавляем значение к имеющемуся списку зарплат.
+    return average(resultAvrSalary) # Результат возвращается уже с расчетом средней зарплаты по возврасту.
+
+print(averageSalaryByAge("rate")) # -> load_file -> averageSalaryByAge
